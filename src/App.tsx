ee7ab7db3d0065e756528709976b54a6bfe94364
blur <thetobiterra@gmail.com>
2026-01-20 17:33:30 +0100

    first query

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
-import { useState } from "react";
-
 import "./App.css";
 import { useQuery } from "@tanstack/react-query";
 
+type Post = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+};
+
+const fetchPosts = async (): Promise<Post[]> => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  if (!res.ok) throw new Error("Error fetching Posts");
+  return res.json();
+};
+
 function App() {
-  return <></>;
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["posts"],
+    queryFn: fetchPosts,
+  });
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+  if (!data) return null;
+  return (
+    <>
+      {data.map((post) => (
+        <p> {post.title} </p>
+      ))}
+    </>
+  );
 }
 
 export default App;
